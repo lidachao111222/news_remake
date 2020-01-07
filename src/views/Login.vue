62640fc0f8df856e00b4702d9289ea2c24bbac0e
chaoli <chao.li@usask.ca>
2020-01-07 13:03:27 +0800

    初步完成登录页面静态配置

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,63 @@
 <template>
-  <div>cccccc</div>
+  <div>
+    <div class="container">
+      <div class="close">
+        <span class="iconfont iconicon-test"></span>
+      </div>
+      <div class="logo">
+        <span class="iconfont iconnew"></span>
+      </div>
+      <div class="inputs">
+        <input data-v-744880be placeholder="请输入手机号" class="input" />
+        <input data-v-744880be placeholder="密码" class="input" type="password" />
+      </div>
+      <p class="tips">
+        没有账号？
+        <a href="#/register" class>去注册</a>
+      </p>
+      <div data-v-4bc01e24 class="button">登录按钮</div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  padding: 20px;
+}
 
+.close {
+  span {
+    font-size: 27 / 360 * 100vw;
+  }
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+
+  span {
+    display: block;
+    font-size: 126 / 360 * 100vw;
+    color: #d81e06;
+  }
+}
+
+.inputs {
+  input {
+    margin-bottom: 20px;
+  }
+}
+
+.tips {
+  text-align: right;
+  margin-bottom: 20px;
+
+  a {
+    color: #3385ff;
+  }
+}
 </style>
